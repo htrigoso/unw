7aaa5d363dce8e30d8428d29f2ac88e22590a55a
Hugo Trigoso Paredes <trigosope@gmail.com>
2025-06-16 13:31:04 -0500

    Update

--- wp-content/themes/unw/templates/template-home.php
+++ wp-content/themes/unw/templates/template-home.php
@@ -10,10 +10,10 @@
 <?php get_header(); ?>
 
 <?php
-  // get_template_part('content-parts/content', 'top-bar');
+  get_template_part('content-parts/content', 'top-bar');
 ?>
 <?php
-// get_template_part('content-parts/content', 'navbar');
+ get_template_part('content-parts/content', 'navbar');
 ?>
 
 <?php
