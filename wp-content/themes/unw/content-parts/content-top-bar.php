1de34c21da3edc3edb8a57d034a1f95c3e4cac57
Hugo Trigoso Paredes <trigosope@gmail.com>
2025-06-16 12:38:25 -0500

    Stop tracking wp-config.php and add to .gitignore

---
+++ wp-content/themes/unw/content-parts/content-top-bar.php
@@ -0,0 +1,42 @@
+<section class="top-bar">
+  <div class="top-bar__wrapper">
+    <div class="top-bar__left">
+      <ul class="top-bar__menu">
+        <li class="top-bar__menu-item"><a class="top-bar__menu-link" href="#">INTERNACIONALIZACIÓN</a></li>
+        <li class="top-bar__menu-item"><a class="top-bar__menu-link" href="#">NOTICIAS</a></li>
+        <li class="top-bar__menu-item"><a class="top-bar__menu-link" href="#">INVESTIGACIÓN</a></li>
+        <li class="top-bar__menu-item"><a class="top-bar__menu-link" href="#">REPOSITORIO</a></li>
+      </ul>
+    </div>
+    <div class="top-bar__right">
+      <div class="top-bar__search">
+        <!-- <input class="top-bar__search-input" type="text" placeholder="¿Qué estás buscando?" />
+        <button class="top-bar__search-button">🔍</button> -->
+      </div>
+      <div class="top-bar__social">
+        <ul class="top-bar__social-list">
+          <li class="top-bar__social-item">
+            <a class="top-bar__social-link" href="">
+              <img class="top-bar__social-icon" src="<?php echo get_template_directory_uri(); ?>/upload/social/facebook.svg" alt="Facebook" />
+            </a>
+          </li>
+          <li class="top-bar__social-item">
+            <a class="top-bar__social-link" href="">
+              <img class="top-bar__social-icon" src="<?php echo get_template_directory_uri(); ?>/upload/social/youtube.svg" alt="YouTube" />
+            </a>
+          </li>
+          <li class="top-bar__social-item">
+            <a class="top-bar__social-link" href="">
+              <img class="top-bar__social-icon" src="<?php echo get_template_directory_uri(); ?>/upload/social/link.svg" alt="LinkedIn" />
+            </a>
+          </li>
+          <li class="top-bar__social-item">
+            <a class="top-bar__social-link" href="">
+              <img class="top-bar__social-icon" src="<?php echo get_template_directory_uri(); ?>/upload/social/instagram.svg" alt="Instagram" />
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</section>
--- wp-content/themes/unw/templates/template-home.php
+++ wp-content/themes/unw/templates/template-home.php
@@ -10,7 +10,10 @@
 <?php get_header(); ?>
 
 <?php
-  // get_template_part('content-parts/content', 'navbar');
+  // get_template_part('content-parts/content', 'top-bar');
+?>
+<?php
+// get_template_part('content-parts/content', 'navbar');
 ?>
 
 <?php
