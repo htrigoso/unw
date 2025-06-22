a3ff21baccb79cde008e313bb29d22510af9b691
Alejandro Jacobo <alejandr.jacobo@gmail.com>
2025-06-22 17:36:09 -0500

    fix: default tab

--- wp-content/themes/unw/app/components/Tabs.js
+++ wp-content/themes/unw/app/components/Tabs.js
@@ -31,7 +31,7 @@ export default class Tabs extends Component {
   }
 
   activateFirstTab() {
-    const firstTab = this.elements.tabItems[7]
+    const firstTab = this.elements.tabItems[0]
     if (!firstTab) return
 
     const targetId = firstTab.dataset.target
