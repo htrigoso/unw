e85f7af345d21d2ab23925dc1764d2a99a337304
Alejandro Jacobo <alejandr.jacobo@gmail.com>
2025-07-01 22:08:20 -0500

    feat: added health title section

--- wp-content/themes/unw/content-parts/pages/faculty/tabs/1-health-science/content-health.php
+++ wp-content/themes/unw/content-parts/pages/faculty/tabs/1-health-science/content-health.php
@@ -1,1 +1,6 @@
-<p>Health</p>
+<div class="health">
+  <div class="health__header">
+    <h1 class="health__header--title">Las carreras de Ciencias de la Salud son <span>reconocidas por su formación innovadora</span></h1>
+  </div>
+
+</div>
