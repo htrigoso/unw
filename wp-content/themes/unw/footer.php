85bd7ed71ae7e06928800f076cfa730cfa218bed
Alejandro Jacobo <alejandr.jacobo@gmail.com>
2025-08-26 11:45:29 -0500

    fix: defer script

--- wp-content/themes/unw/footer.php
+++ wp-content/themes/unw/footer.php
@@ -2,8 +2,6 @@
 <?php get_template_part('content-parts/content', 'toaster'); ?>
 <?php get_template_part('content-parts/content', 'footer'); ?>
 <?php wp_footer(); ?>
-<!-- TODO: Add globe.gl script through action -->
-<script type="text/javascript" src="//cdn.jsdelivr.net/npm/globe.gl" id="globe-gl-js"></script>
 </body>
 
 </html>
