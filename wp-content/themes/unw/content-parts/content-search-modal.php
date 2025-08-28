84513b673f535cbd3fc48a29099b6297ed706f95
Alejandro Jacobo <alejandr.jacobo@gmail.com>
2025-08-28 11:38:46 -0500

    feat: added example to video slide

---
+++ wp-content/themes/unw/content-parts/content-search-modal.php
@@ -0,0 +1,18 @@
+<div class="search-modal__wrapper">
+  <div class="search-modal__wrapper__content">
+    <div class="search-modal">
+      <h4>Busca aquí</h4>
+      <div>
+        <div class="search-field">
+          <input type="search" />
+          <i>
+            <svg width="24" height="24">
+              <use xlink:href="#search"></use>
+            </svg>
+          </i>
+        </div>
+        <button>Buscar</button>
+      </div>
+    </div>
+  </div>
+</div>
