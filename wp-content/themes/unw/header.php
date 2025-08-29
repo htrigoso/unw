02aef04032715c5de8ba1a9ceaed55004b426295
Hugo Trigoso Paredes <trigosope@gmail.com>
2025-08-29 10:06:26 -0500

    Update fixes

--- wp-content/themes/unw/header.php
+++ wp-content/themes/unw/header.php
@@ -90,26 +90,26 @@
 
   <!-- Load fonts-->
   <!-- Load Founders Grotesk fonts -->
-  <link href="/wp-content/themes/unw/assets/fonts/FoundersGrotesk-Bold.woff2" rel="preload" as="font" type="font/woff2"
-    crossorigin>
-  <link href="/wp-content/themes/unw/assets/fonts/FoundersGrotesk-BoldItalic.woff2" rel="preload" as="font"
-    type="font/woff2" crossorigin>
-  <link href="/wp-content/themes/unw/assets/fonts/FoundersGrotesk-Semibold.woff2" rel="preload" as="font"
-    type="font/woff2" crossorigin>
-  <link href="/wp-content/themes/unw/assets/fonts/FoundersGrotesk-SemiboldItalic.woff2" rel="preload" as="font"
-    type="font/woff2" crossorigin>
-  <link href="/wp-content/themes/unw/assets/fonts/FoundersGrotesk-Medium.woff2" rel="preload" as="font"
-    type="font/woff2" crossorigin>
-  <link href="/wp-content/themes/unw/assets/fonts/FoundersGrotesk-MediumItalic.woff2" rel="preload" as="font"
-    type="font/woff2" crossorigin>
-  <link href="/wp-content/themes/unw/assets/fonts/FoundersGrotesk-Regular.woff2" rel="preload" as="font"
-    type="font/woff2" crossorigin>
-  <link href="/wp-content/themes/unw/assets/fonts/FoundersGrotesk-RegularItalic.woff2" rel="preload" as="font"
-    type="font/woff2" crossorigin>
-  <link href="/wp-content/themes/unw/assets/fonts/FoundersGrotesk-Light.woff2" rel="preload" as="font" type="font/woff2"
-    crossorigin>
-  <link href="/wp-content/themes/unw/assets/fonts/FoundersGrotesk-LightItalic.woff2" rel="preload" as="font"
-    type="font/woff2" crossorigin>
+  <link href="<?php echo get_template_directory_uri(); ?>/assets/fonts/FoundersGrotesk-Bold.woff2" rel="preload"
+    as="font" type="font/woff2" crossorigin>
+  <link href="<?php echo get_template_directory_uri(); ?>/assets/fonts/FoundersGrotesk-BoldItalic.woff2" rel="preload"
+    as="font" type="font/woff2" crossorigin>
+  <link href="<?php echo get_template_directory_uri(); ?>/assets/fonts/FoundersGrotesk-Semibold.woff2" rel="preload"
+    as="font" type="font/woff2" crossorigin>
+  <link href="<?php echo get_template_directory_uri(); ?>/assets/fonts/FoundersGrotesk-SemiboldItalic.woff2"
+    rel="preload" as="font" type="font/woff2" crossorigin>
+  <link href="<?php echo get_template_directory_uri(); ?>/assets/fonts/FoundersGrotesk-Medium.woff2" rel="preload"
+    as="font" type="font/woff2" crossorigin>
+  <link href="<?php echo get_template_directory_uri(); ?>/assets/fonts/FoundersGrotesk-MediumItalic.woff2" rel="preload"
+    as="font" type="font/woff2" crossorigin>
+  <link href="<?php echo get_template_directory_uri(); ?>/assets/fonts/FoundersGrotesk-Regular.woff2" rel="preload"
+    as="font" type="font/woff2" crossorigin>
+  <link href="<?php echo get_template_directory_uri(); ?>/assets/fonts/FoundersGrotesk-RegularItalic.woff2"
+    rel="preload" as="font" type="font/woff2" crossorigin>
+  <link href="<?php echo get_template_directory_uri(); ?>/assets/fonts/FoundersGrotesk-Light.woff2" rel="preload"
+    as="font" type="font/woff2" crossorigin>
+  <link href="<?php echo get_template_directory_uri(); ?>/assets/fonts/FoundersGrotesk-LightItalic.woff2" rel="preload"
+    as="font" type="font/woff2" crossorigin>
   <script>
   window.appConfigUnw = {
     themeUrl: "<?= get_template_directory_uri(); ?>",
