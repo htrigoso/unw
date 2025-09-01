22396fd580442b63cf91415e1d5c4063622245e4
Alejandro Jacobo <alejandr.jacobo@gmail.com>
2025-08-31 19:41:08 -0500

    feat: all careers swiper

--- wp-content/themes/unw/templates/template-all-careers.php
+++ wp-content/themes/unw/templates/template-all-careers.php
@@ -15,11 +15,25 @@
   $current_id_term = get_current_term_id();
   $images = get_field('images', 'facultad_' . $current_id_term);
 
-  if (   $current_id_term ) {
-    /// vdebug(get_field('hero'));
-  }else {
-  // vdebug($images);
+  $hero_title = '';
+  $hero_img_desktop = '';
+  $hero_img_mobile = '';
+
+  if ($current_id_term == 0) {
+    $hero_data = get_field('hero');
+    if (!empty($hero_data)) {
+      $hero_title = $hero_data['title'] ?: $hero_title;
+      $hero_img_desktop = $hero_data['images']['desktop']['url'] ?: $hero_img_desktop;
+      $hero_img_mobile = $hero_data['images']['mobile']['url'] ?: $hero_img_mobile;
+    }
+  } else {
+    if (!empty($images)) {
+      $hero_img_desktop = $images['desktop']['url'] ?: $hero_img_desktop;
+      $hero_img_mobile = $images['mobile']['url'] ?: $hero_img_mobile;
+    }
   }
+
+
   $facultades = get_terms([
     'taxonomy'   => 'facultad',
     'hide_empty' => false,
@@ -53,10 +67,10 @@
   ];
   ?>
   <?php get_template_part(COMMON_CONTENT_PATH, 'hero-slide', [
-    'img_desktop' => UPLOAD_PATH . '/all-careers/all-hero-mobile.jpg',
-    'img_mobile'  => UPLOAD_PATH . '/all-careers/all-hero-mobile.jpg',
-    'alt'         => 'Todas las carreras',
-    'title'       => 'Todas las carreras',
+    'img_desktop' => $hero_img_desktop,
+    'img_mobile'  => $hero_img_mobile,
+    'alt'         => $hero_title,
+    'title'       => $hero_title,
     'breadcrumbs' => $breadcrumb,
     'variant'     => 'primary'
   ]); ?>
