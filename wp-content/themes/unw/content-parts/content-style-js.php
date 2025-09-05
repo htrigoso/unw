4996bcae2f8ea21e220cf91c5038c006314521e1
Hugo Trigoso Paredes <trigosope@gmail.com>
2025-09-04 21:01:27 -0500

    Config preload js and css

---
+++ wp-content/themes/unw/content-parts/content-style-js.php
@@ -0,0 +1,56 @@
+<?php
+
+  global $wp_query;
+
+  $assetsJsonFile = "";
+  if (file_exists(get_template_directory() . '/build/assets.json')) {
+    $assetsJsonFile = file_get_contents(get_template_directory() . '/build/assets.json');
+  } elseif (file_exists(get_template_directory() . '/public/assets.json')) {
+    $assetsJsonFile = file_get_contents(get_template_directory() . '/public/assets.json');
+  }
+
+  if (!empty($assetsJsonFile)) {
+    $assets = json_decode($assetsJsonFile, true);
+    $vars = $wp_query->query_vars;
+    $themePath = get_template_directory_uri();
+    $env = $assets['env'];
+    unset($assets['env']);
+  }
+
+
+
+
+  if (!empty($assets)) {
+    foreach ($assets as $key => $val) {
+      switch ($key) {
+        case 'app':
+          {
+            if (array_key_exists('js', $val)) {
+              $style_url = ($env === 'production') ? $themePath . '/' . $val['css'] : null;
+              $script_url = ($env === 'production') ? $themePath . '/' . $val['js'] : $val['js'];
+
+              echo "<link rel='preload' href='{$style_url}' as='style' onload=\"this.onload=null;this.rel='stylesheet'\" media='all'>\n";
+              echo "<noscript><link rel='stylesheet' href='{$style_url}' media='all'></noscript>\n";
+
+              echo "<script type='module' src='{$script_url}'></script>";
+            }
+          }
+          break;
+        case $vars['ASSETS_CHUNK_NAME']:
+          {
+            if (array_key_exists('js', $val)) {
+              $style_url = ($env === 'production') ? $themePath . '/' . $val['css'] : null;
+              $script_url = ($env === 'production') ? $themePath . '/' . $val['js'] : $val['js'];
+
+              echo "<link rel='stylesheet' href='{$style_url}' media='print' onload=\"this.media='all'\">\n";
+              echo "<noscript><link rel='stylesheet' href='{$style_url}' media='all'></noscript>\n";
+
+              echo "<link rel='modulepreload' href='" . esc_url($script_url) . "'>\n";
+              echo "<script type='module'>import('" . esc_url($script_url) . "');</script>\n";
+
+            }
+          }
+          break;
+      }
+    }
+  }
