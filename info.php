d6d8862923da68e6f2e6a2e8b920539d716c9e8e
Hugo Trigoso Paredes <trigosope@gmail.com>
2025-09-10 17:27:14 -0500

    Update fixes

--- info.php
+++ info.php
@@ -1,22 +1,4 @@
 <?php
-// require __DIR__ . '/wp-load.php';
-// $args = array(
-//   'post_type'      => 'curso',
-//   'posts_per_page' => -1,
-//   'meta_query'     => array(
-//     array(
-//       'key'     => '_thumbnail_id',
-//       'compare' => 'EXISTS'
-//     )
-//   )
-// );
+require __DIR__ . '/wp-load.php';
 
-// $cursos = new WP_Query($args);
-
-// if ($cursos->have_posts()) {
-//   while ($cursos->have_posts()) {
-//     $cursos->the_post();
-//     delete_post_thumbnail(get_the_ID());
-//   }
-// }
-// wp_reset_postdata();
+vdebug(get_carreras_campus_indexado());
