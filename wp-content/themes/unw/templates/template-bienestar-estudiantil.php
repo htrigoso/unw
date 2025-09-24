a1b6560b6d71b730d6e20e7d5f97f57af1232528
Hugo Trigoso Paredes <trigosope@gmail.com>
2025-09-24 16:38:26 -0500

    Add integration bienestart estudiantil

--- wp-content/themes/unw/templates/template-bienestar-estudiantil.php
+++ wp-content/themes/unw/templates/template-bienestar-estudiantil.php
@@ -8,6 +8,9 @@
 <?php set_query_var('NAVBAR_COLOR', ''); ?>
 <?php
   get_header();
+   $acf_sidebar = get_field('sidebar');
+  $acf_cards = get_field('items');
+
 ?>
 
 
@@ -16,50 +19,18 @@
 <main>
   <div class="main_container">
     <div class="info_page">
-      <div class="cover_img_page center">
-        <div class="overlay"></div>
-        <img src="<?= UPLOAD_MIGRATION_PATH . '/bienestar-estudiantil/direccion-bienestar-universitario.png' ?>"
-          srcset="<?= UPLOAD_MIGRATION_PATH . '/bienestar-estudiantil/direccion-bienestar-universitario.png' ?> 500w, <?= UPLOAD_MIGRATION_PATH . '/bienestar-estudiantil/direccion-bienestar-universitario.png' ?> 1920w"
-          sizes="100vw" alt="" class="img_cover">
-        <div id="presentacion_vf" class="info_cover_page center">
-          <div  class="container">
-            <h2 class="categoria_page serv_uni">Servicios universitarios</h2>
-            <h2 class="h1_carreras">Bienestar Estudiantil</h2>
-          </div>
-        </div>
-        <div class="miga_de_pan">
-          <div class="container">
-            <div class="content_links_miga"><a href="<?= home_url("/") ?>" class="link miga">Inicio /</a><a
-                href="<?= home_url("/servicios-universitarios/") ?>" class="link miga">Servicios
-                universitarios&nbsp;/</a><a href="#" aria-current="page" class="link miga w--current">Bienestar
-                Estudiantil</a></div>
-          </div>
-        </div>
-      </div>
+      <?php
+          get_template_part('content-parts/components/info-cover');
+        ?>
       <div class="main_page">
         <div class="page_interna">
           <div class="container full">
             <div class="_2-col">
               <div class="col-1 full">
                 <div class="tabs_menu notab serv_uni secretaria">
-                  <a href="#presentacion"
-                    class="link_item_tab w-inline-block">
-                    <div>Presentación</div>
-                  </a>
-
-
-
-                  <!-- Sección del Post Bienestar Estudiantil y Becas del CPT Bienestar Universitario - Hernán Chira - Parte 2 - INICIO -->
-                  <a href="#serviciosu-bienestar-estudiantil"
-                    class="link_item_tab w-inline-block">
-                    <div>Servicios</div>
-                  </a>
-                  <!-- Sección del Post Bienestar Estudiantil y Becas del CPT Bienestar Universitario - Hernán Chira - Parte 2 - FIN -->
-
-
-                  <a href="#" class="link_item_tab hide w-inline-block">
-                    <div>Prewiener</div>
-                  </a>
+                  <?php
+                    get_template_part('content-parts/components/sidebar', null, ['sidebar' => $acf_sidebar['sidebar_items']]);
+                  ?>
                 </div>
               </div>
               <div class="col-1 full right">
@@ -70,19 +41,9 @@
 
                         <!-- Sección del Post Bienestar Estudiantil y Becas del CPT Bienestar Universitario - Hernán Chira - Parte 3 - INICIO -->
                         <div class="clase_para_wordpress richt_text " id="presentacion">
-                          <h2>Presentación</h2>
-                          <p>BIENESTAR ESTUDIANTIL está integrada por un equipo multidisciplinario de profesionales,
-                            cuyo propósito es promover el bienestar físico, mental y moral de la comunidad
-                            universitaria, a través de programas y servicios que estimulan su desempeño y
-                            desenvolvimiento personal y académico, contribuyendo al desarrollo integral de su vida
-                            universitaria.</p>
-                          <p>El área de bienestar estudiantil brinda sus servicios a través del Área de Servicios al
-                            estudiante (Servicios Médicos y Servicios Psicopedagógicos) y el Área de Vida Universitaria
-                            (Promoción del&nbsp;Deporte y Promoción Cultural). Además, organiza y lidera actividades
-                            como charlas, talleres,&nbsp;conferencias, conversatorios, shows artísticos y campeonatos
-                            deportivos.</p>
-                          <p>A continuación, te presentamos los servicios de Bienestar Estudiantil, ¡comprometidos con
-                            el desarrollo integral de sus alumnos!</p>
+                          <?php
+                              the_content();
+                            ?>
                         </div>
                         <!-- Sección del Post Bienestar Estudiantil y Becas del CPT Bienestar Universitario - Hernán Chira - Parte 3 - INICIO -->
 
@@ -95,47 +56,15 @@
 								<h3 class="h3_interna_title">Bienestar Estudiantil</h3>
                                 <div class="line"></div>
                             </div> -->
-
-
                             <div id="serviciosu-bienestar-estudiantil" role="list"
                               class="collection_list gilla _3-col w-dyn-items">
-                              <div role="listitem" class="collection_item serv_univer w-dyn-item">
-                                <a title="Ver más sobre Promoción Cultural"
-                                  href="<?= home_url("/bienestar-universitario/promocion-cultural/") ?>"
-                                  class="item_serv_university w-inline-block">
-                                  <!-- <div class="tipo_servicio">Bienestar Estudiantil</div> -->
-                                  <h4 class="h4_light">Promoción Cultural</h4>
-                                  <div class="btn-legacy text">Ver más</div>
-                                </a>
-                              </div>
-                              <div role="listitem" class="collection_item serv_univer w-dyn-item">
-                                <a title="Ver más sobre Promoción del deporte"
-                                  href="<?= home_url("/bienestar-universitario/promocion-del-deporte/") ?>"
-                                  class="item_serv_university w-inline-block">
-                                  <!-- <div class="tipo_servicio">Bienestar Estudiantil</div> -->
-                                  <h4 class="h4_light">Promoción del deporte</h4>
-                                  <div class="btn-legacy text">Ver más</div>
-                                </a>
-                              </div>
-                              <div role="listitem" class="collection_item serv_univer w-dyn-item">
-                                <a title="Ver más sobre Servicios psicopedagógicos"
-                                  href="<?= home_url("/bienestar-universitario/servicios-psicopedagogicos/") ?>"
-                                  class="item_serv_university w-inline-block">
-                                  <!-- <div class="tipo_servicio">Bienestar Estudiantil</div> -->
-                                  <h4 class="h4_light">Servicios psicopedagógicos</h4>
-                                  <div class="btn-legacy text">Ver más</div>
-                                </a>
-                              </div>
-                              <div role="listitem" class="collection_item serv_univer w-dyn-item">
-                                <a title="Ver más sobre Servicios médicos"
-                                  href="<?= home_url("/bienestar-universitario/servicios-medicos/") ?>"
-                                  class="item_serv_university w-inline-block">
-                                  <!-- <div class="tipo_servicio">Bienestar Estudiantil</div> -->
-                                  <h4 class="h4_light">Servicios médicos</h4>
-                                  <div class="btn-legacy text">Ver más</div>
-                                </a>
-                              </div>
+                              <?php
+                                get_template_part('content-parts/components/cards', null, ['cards' => $acf_cards['cards']]);
+                              ?>
                             </div>
+
+
+
                           </div>
 
                         </div>
