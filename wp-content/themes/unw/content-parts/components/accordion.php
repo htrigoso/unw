2a1c9620d343c3def70fe8deb8299d157df34c53
Hugo Trigoso Paredes <trigosope@gmail.com>
2025-09-27 08:16:47 -0500

    Update integration all page

--- wp-content/themes/unw/content-parts/components/accordion.php
+++ wp-content/themes/unw/content-parts/components/accordion.php
@@ -1,17 +1,25 @@
 <?php
-$accordion = $args['accordion'] ?? [];
-$class_faq = $args['class_faq'] ?? '';
+$accordion     = $args['accordion'] ?? [];
+$class_faq     = $args['class_faq'] ?? '';
+$active_index  = $args['active_index'] ?? null; // null si no se envía
 
 if ($accordion): ?>
 <?php foreach ($accordion as $i => $item):
     $data_id = 'accordion-' . md5($item['accordion_title'] . $i . uniqid('', true));
+
+    // Mostrar numeración si $active_index es null (no enviado) o es un número
+    $show_prefix  = ($active_index === null || is_numeric($active_index));
+    $title_prefix = $show_prefix ? (($i + 1) . '. ') : '';
+
+    // Activar acordeón si el índice coincide con $active_index
+    $is_active = (is_numeric($active_index) && $i === (int)$active_index);
   ?>
-<div class="acordeon">
+<div class="acordeon <?= $is_active ? 'is-open' : '' ?>">
   <div data-w-id="<?php echo esc_attr($data_id); ?>" class="trigger_acordeon">
     <a href="#" class="linktransparencia w-inline-block">
       <img src="images/icon_link.svg" alt="" class="icon_link">
       <h4 class="<?= esc_attr($class_faq) ? $class_faq : 'h4_admin' ?>">
-        <?php echo ($i + 1) . '. ' . esc_html($item['accordion_title']); ?>
+        <?php echo $title_prefix . esc_html($item['accordion_title']); ?>
         <br>
       </h4>
     </a>
@@ -20,7 +28,7 @@ if ($accordion): ?>
     </div>
   </div>
 
-  <div style="height: 0px;" class="content_acordeon">
+  <div style="height: <?= $is_active ? 'auto' : '0px' ?>;" class="content_acordeon">
     <div class="clase_para_wordpress transparencia">
       <?php echo wp_kses_post($item['accordion_content']); ?>
     </div>
