5f0fd53723a808e6323b001e4daa3e0d4c93a8a2
Hugo Trigoso Paredes <trigosope@gmail.com>
2025-09-28 17:36:14 -0500

    Update fonts

--- wp-content/themes/unw/content-parts/content-fonts.php
+++ wp-content/themes/unw/content-parts/content-fonts.php
@@ -8,11 +8,11 @@
   type="font/woff2" crossorigin>
 <link href="<?= THEME_PATH; ?>/assets/fonts/FoundersGrotesk-Medium.woff2" rel="preload" as="font" type="font/woff2"
   crossorigin>
-<link href="<?= THEME_PATH; ?>/assets/fonts/FoundersGrotesk-MediumItalic.woff2" rel="preload" as="font" type="font/woff2"
+<link href="<?= THEME_PATH; ?>/assets/fonts/FoundersGrotesk-MediumItalic.woff2" rel="preload" as="font"
+  type="font/woff2" crossorigin>
+<link href="<?= THEME_PATH; ?>/assets/fonts/FoundersGrotesk-Regular-v2.woff2" rel="preload" as="font" type="font/woff2"
   crossorigin>
-<link href="<?= THEME_PATH; ?>/assets/fonts/FoundersGrotesk-Regular.woff2" rel="preload" as="font" type="font/woff2"
-  crossorigin>
-<link href="<?= THEME_PATH; ?>/assets/fonts/FoundersGrotesk-RegularItalic.woff2" rel="preload" as="font"
+<link href="<?= THEME_PATH; ?>/assets/fonts/FoundersGrotesk-RegularItalic-v2.woff2" rel="preload" as="font"
   type="font/woff2" crossorigin>
 <link href="<?= THEME_PATH; ?>/assets/fonts/FoundersGrotesk-Light.woff2" rel="preload" as="font" type="font/woff2"
   crossorigin>
@@ -23,93 +23,93 @@
 
 
 <style>
-  @font-face {
-    font-family: "Founders Grotesk";
-    src: url("<?= THEME_PATH; ?>/assets/fonts/FoundersGrotesk-Bold.woff2") format("woff2"),
-      url("<?= THEME_PATH; ?>/assets/fonts/FoundersGrotesk-Bold.woff") format("woff");
-    font-weight: bold;
-    font-style: normal;
-    font-display: swap;
-  }
+@font-face {
+  font-family: "Founders Grotesk";
+  src: url("<?= THEME_PATH; ?>/assets/fonts/FoundersGrotesk-Bold.woff2") format("woff2"),
+    url("<?= THEME_PATH; ?>/assets/fonts/FoundersGrotesk-Bold.woff") format("woff");
+  font-weight: bold;
+  font-style: normal;
+  font-display: swap;
+}
 
-  @font-face {
-    font-family: "Founders Grotesk";
-    src: url("<?= THEME_PATH; ?>/assets/fonts/FoundersGrotesk-BoldItalic.woff2") format("woff2"),
-      url("<?= THEME_PATH; ?>/assets/fonts/FoundersGrotesk-BoldItalic.woff") format("woff");
-    font-weight: bold;
-    font-style: italic;
-    font-display: swap;
-  }
+@font-face {
+  font-family: "Founders Grotesk";
+  src: url("<?= THEME_PATH; ?>/assets/fonts/FoundersGrotesk-BoldItalic.woff2") format("woff2"),
+    url("<?= THEME_PATH; ?>/assets/fonts/FoundersGrotesk-BoldItalic.woff") format("woff");
+  font-weight: bold;
+  font-style: italic;
+  font-display: swap;
+}
 
-  @font-face {
-    font-family: "Founders Grotesk";
-    src: url("<?= THEME_PATH; ?>/assets/fonts/FoundersGrotesk-Semibold.woff2") format("woff2"),
-      url("<?= THEME_PATH; ?>/assets/fonts/FoundersGrotesk-Semibold.woff") format("woff");
-    font-weight: 600;
-    font-style: normal;
-    font-display: swap;
-  }
+@font-face {
+  font-family: "Founders Grotesk";
+  src: url("<?= THEME_PATH; ?>/assets/fonts/FoundersGrotesk-Semibold.woff2") format("woff2"),
+    url("<?= THEME_PATH; ?>/assets/fonts/FoundersGrotesk-Semibold.woff") format("woff");
+  font-weight: 600;
+  font-style: normal;
+  font-display: swap;
+}
 
-  @font-face {
-    font-family: "Founders Grotesk";
-    src: url("<?= THEME_PATH; ?>/assets/fonts/FoundersGrotesk-SemiboldItalic.woff2") format("woff2"),
-      url("<?= THEME_PATH; ?>/assets/fonts/FoundersGrotesk-SemiboldItalic.woff") format("woff");
-    font-weight: 600;
-    font-style: italic;
-    font-display: swap;
-  }
+@font-face {
+  font-family: "Founders Grotesk";
+  src: url("<?= THEME_PATH; ?>/assets/fonts/FoundersGrotesk-SemiboldItalic.woff2") format("woff2"),
+    url("<?= THEME_PATH; ?>/assets/fonts/FoundersGrotesk-SemiboldItalic.woff") format("woff");
+  font-weight: 600;
+  font-style: italic;
+  font-display: swap;
+}
 
-  @font-face {
-    font-family: "Founders Grotesk";
-    src: url("<?= THEME_PATH; ?>/assets/fonts/FoundersGrotesk-Medium.woff2") format("woff2"),
-      url("<?= THEME_PATH; ?>/assets/fonts/FoundersGrotesk-Medium.woff") format("woff");
-    font-weight: 500;
-    font-style: normal;
-    font-display: swap;
-  }
+@font-face {
+  font-family: "Founders Grotesk";
+  src: url("<?= THEME_PATH; ?>/assets/fonts/FoundersGrotesk-Medium.woff2") format("woff2"),
+    url("<?= THEME_PATH; ?>/assets/fonts/FoundersGrotesk-Medium.woff") format("woff");
+  font-weight: 500;
+  font-style: normal;
+  font-display: swap;
+}
 
-  @font-face {
-    font-family: "Founders Grotesk";
-    src: url("<?= THEME_PATH; ?>/assets/fonts/FoundersGrotesk-MediumItalic.woff2") format("woff2"),
-      url("<?= THEME_PATH; ?>/assets/fonts/FoundersGrotesk-MediumItalic.woff") format("woff");
-    font-weight: 500;
-    font-style: italic;
-    font-display: swap;
-  }
+@font-face {
+  font-family: "Founders Grotesk";
+  src: url("<?= THEME_PATH; ?>/assets/fonts/FoundersGrotesk-MediumItalic.woff2") format("woff2"),
+    url("<?= THEME_PATH; ?>/assets/fonts/FoundersGrotesk-MediumItalic.woff") format("woff");
+  font-weight: 500;
+  font-style: italic;
+  font-display: swap;
+}
 
-  @font-face {
-    font-family: "Founders Grotesk";
-    src: url("<?= THEME_PATH; ?>/assets/fonts/FoundersGrotesk-Regular-v2.woff2") format("woff2"),
-      url("<?= THEME_PATH; ?>/assets/fonts/FoundersGrotesk-Regular-v2.woff") format("woff");
-    font-weight: normal;
-    font-style: normal;
-    font-display: swap;
-  }
+@font-face {
+  font-family: "Founders Grotesk";
+  src: url("<?= THEME_PATH; ?>/assets/fonts/FoundersGrotesk-Regular-v2.woff2") format("woff2"),
+    url("<?= THEME_PATH; ?>/assets/fonts/FoundersGrotesk-Regular-v2.woff") format("woff");
+  font-weight: normal;
+  font-style: normal;
+  font-display: swap;
+}
 
-  @font-face {
-    font-family: "Founders Grotesk";
-    src: url("<?= THEME_PATH; ?>/assets/fonts/FoundersGrotesk-RegularItalic-v2.woff2") format("woff2"),
-      url("<?= THEME_PATH; ?>/assets/fonts/FoundersGrotesk-RegularItalic-v2.woff") format("woff");
-    font-weight: normal;
-    font-style: italic;
-    font-display: swap;
-  }
+@font-face {
+  font-family: "Founders Grotesk";
+  src: url("<?= THEME_PATH; ?>/assets/fonts/FoundersGrotesk-RegularItalic-v2.woff2") format("woff2"),
+    url("<?= THEME_PATH; ?>/assets/fonts/FoundersGrotesk-RegularItalic-v2.woff") format("woff");
+  font-weight: normal;
+  font-style: italic;
+  font-display: swap;
+}
 
-  @font-face {
-    font-family: "Founders Grotesk";
-    src: url("<?= THEME_PATH; ?>/assets/fonts/FoundersGrotesk-Light.woff2") format("woff2"),
-      url("<?= THEME_PATH; ?>/assets/fonts/FoundersGrotesk-Light.woff") format("woff");
-    font-weight: 300;
-    font-style: normal;
-    font-display: swap;
-  }
+@font-face {
+  font-family: "Founders Grotesk";
+  src: url("<?= THEME_PATH; ?>/assets/fonts/FoundersGrotesk-Light.woff2") format("woff2"),
+    url("<?= THEME_PATH; ?>/assets/fonts/FoundersGrotesk-Light.woff") format("woff");
+  font-weight: 300;
+  font-style: normal;
+  font-display: swap;
+}
 
-  @font-face {
-    font-family: "Founders Grotesk";
-    src: url("<?= THEME_PATH; ?>/assets/fonts/FoundersGrotesk-LightItalic.woff2") format("woff2"),
-      url("<?= THEME_PATH; ?>/assets/fonts/FoundersGrotesk-LightItalic.woff") format("woff");
-    font-weight: 300;
-    font-style: italic;
-    font-display: swap;
-  }
+@font-face {
+  font-family: "Founders Grotesk";
+  src: url("<?= THEME_PATH; ?>/assets/fonts/FoundersGrotesk-LightItalic.woff2") format("woff2"),
+    url("<?= THEME_PATH; ?>/assets/fonts/FoundersGrotesk-LightItalic.woff") format("woff");
+  font-weight: 300;
+  font-style: italic;
+  font-display: swap;
+}
 </style>
