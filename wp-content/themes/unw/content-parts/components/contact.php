56b7541f6808a5056a2d6ff802392ae6084464db
Alejandro Jacobo <alejandr.jacobo@gmail.com>
2025-09-28 16:07:38 -0500

    fix: overflow text

--- wp-content/themes/unw/content-parts/components/contact.php
+++ wp-content/themes/unw/content-parts/components/contact.php
@@ -1,10 +1,12 @@
 <?php
-$contact = $args['contact'] ?? null;
-if ($contact): ?>
-<div class="">
-  <h4 class="h4_verde"><?=esc_html($contact['title'])?></h4>
-  <div class="item_contact last">
-    <div><?= wp_kses_post($contact['description']); ?></div>
+$contact = $args['contact'];
+if (isset($contact)): ?>
+  <div class="">
+    <?php if (!empty($contact['title'])) { ?>
+      <h4 class="h4_verde"><?= esc_html($contact['title']) ?></h4>
+    <?php } ?>
+    <div class="item_contact last">
+      <div><?= wp_kses_post($contact['description']); ?></div>
+    </div>
   </div>
-</div>
 <?php endif; ?>
--- wp-content/themes/unw/templates/template-centro-de-idiomas.php
+++ wp-content/themes/unw/templates/template-centro-de-idiomas.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Template Name: Centros de Idiomas
  */
@@ -7,21 +8,21 @@
 <?php set_query_var('ASSETS_CHUNK_NAME', 'migration'); ?>
 <?php set_query_var('NAVBAR_COLOR', ''); ?>
 <?php
-  get_header();
-  $acf_sidebar = get_field('sidebar');
-  $acf_faq = get_field('faq');
-  $acf_contact = get_field('contact');
+get_header();
+$acf_sidebar = get_field('sidebar');
+$acf_faq = get_field('faq');
+$acf_contact = get_field('contact');
 ?>
 
-<?php get_template_part(GENERAL_CONTENT_PATH, 'navbar');?>
+<?php get_template_part(GENERAL_CONTENT_PATH, 'navbar'); ?>
 <main>
   <div class="main_container">
     <div class="info_page">
       <?php
-          get_template_part('content-parts/components/info-cover', null, [
-            'title_extra'=> 'Becas Externas'
-          ]);
-        ?>
+      get_template_part('content-parts/components/info-cover', null, [
+        'title_extra' => 'Becas Externas'
+      ]);
+      ?>
       <div class="main_page">
         <div class="page_interna">
           <div class="container full">
@@ -29,7 +30,7 @@
               <div class="col-1 full">
                 <div class="tabs_menu notab serv_uni secretaria">
                   <?php
-                    get_template_part('content-parts/components/sidebar', null, ['sidebar' => $acf_sidebar['sidebar_items']]);
+                  get_template_part('content-parts/components/sidebar', null, ['sidebar' => $acf_sidebar['sidebar_items']]);
                   ?>
                 </div>
               </div>
@@ -43,8 +44,8 @@
                             <!-- ichtidiomas w-richtext -->
 
                             <?php
-                              the_content();
-                      ?>
+                            the_content();
+                            ?>
 
 
 
@@ -54,21 +55,23 @@
                         </div>
                         <div class="content_section pr" id="idiomas">
                           <?php
-                                  get_template_part('content-parts/components/accordion', null, ['accordion' => $acf_faq['accordion_items'], 'class_faq'=>'h4_faq']);
-                                ?>
+                          get_template_part('content-parts/components/accordion', null, ['accordion' => $acf_faq['accordion_items'], 'class_faq' => 'h4_faq']);
+                          ?>
 
 
                         </div>
                       </div>
                     </div>
-                    <div class="contact_box servicioswiener">
-                      <?php
-                          get_template_part('content-parts/components/contact', null , [
-                            'contact' => $acf_contact
-                          ]);
+                    <?php if (isset($acf_contact)) { ?>
+                      <div class="contact_box servicioswiener">
+                        <?php
+                        get_template_part('content-parts/components/contact', null, [
+                          'contact' => $acf_contact
+                        ]);
                         ?>
 
-                    </div>
+                      </div>
+                    <?php } ?>
                   </div>
                 </div>
               </div>
