74fae55ef54a2b4ee9da1b2949e7adbc1b9ac298
Hugo Trigoso Paredes <trigosope@gmail.com>
2025-10-02 20:21:48 -0500

    Update template landing

--- wp-content/themes/unw/content-parts/pages/landing/sections/content-content.php
+++ wp-content/themes/unw/content-parts/pages/landing/sections/content-content.php
@@ -2,4 +2,4 @@
 <?php
 $data = get_query_var('section_data', []); // Si no hay data, retorna []
 $content = $data['content'] ?? '';              // Si no hay title, retorna ''
- //vdebug($content);
+vdebug($content);
--- wp-content/themes/unw/content-parts/pages/landing/sections/content-hero.php
+++ wp-content/themes/unw/content-parts/pages/landing/sections/content-hero.php
@@ -3,4 +3,3 @@
 $data = get_query_var('section_data', []); // Si no hay data, retorna []
 $title = $data['title'] ?? '';              // Si no hay title, retorna ''
 $images = $data['images'] ?? [];
-// vdebug($images);
