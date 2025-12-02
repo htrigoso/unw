37a2c7313497c5d527268f24301c8cef2095ad3f
Alejandro M. <alejandr.jacobo@gmail.com>
2025-12-02 08:57:39 -0500

    fix: overflow mobile tabs

--- wp-content/themes/unw/templates/template-admision-beca18.php
+++ wp-content/themes/unw/templates/template-admision-beca18.php
@@ -16,8 +16,8 @@ $acf_sidebar = get_field('sidebar');
 <main>
   <div class="main_container">
     <?php
-          get_template_part('content-parts/components/info-cover');
-        ?>
+    get_template_part('content-parts/components/info-cover');
+    ?>
     <div class="main_page">
       <div class="page_interna">
         <div class="container full">
@@ -27,7 +27,7 @@ $acf_sidebar = get_field('sidebar');
                 <div class="wrapper_collection w-dyn-list">
                   <div role="list" class="collection_list admin w-dyn-items">
                     <?php
-                      get_template_part('content-parts/components/sidebar', null, ['sidebar' => $acf_sidebar['sidebar_items']]);
+                    get_template_part('content-parts/components/sidebar', null, ['sidebar' => $acf_sidebar['sidebar_items']]);
                     ?>
                   </div>
                 </div>
@@ -38,9 +38,9 @@ $acf_sidebar = get_field('sidebar');
                 <div class="section_tab _2-col">
                   <div class="content_seccion_tab">
                     <div class="info_section nopadding">
-                        <?php
-                              the_content();
-                            ?>
+                      <?php
+                      the_content();
+                      ?>
                     </div>
                   </div>
 
--- wp-content/themes/unw/templates/template-admision-examen-admision.php
+++ wp-content/themes/unw/templates/template-admision-examen-admision.php
@@ -8,8 +8,8 @@
 <?php set_query_var('ASSETS_CHUNK_NAME', 'migration'); ?>
 <?php set_query_var('NAVBAR_COLOR', ''); ?>
 <?php
-  $acf_sidebar = get_field('sidebar');
-  $acf_examen = get_field('examen');
+$acf_sidebar = get_field('sidebar');
+$acf_examen = get_field('examen');
 get_header();
 ?>
 
@@ -17,27 +17,29 @@ get_header();
 <main>
   <div class="main_container">
     <?php
-          get_template_part('content-parts/components/info-cover');
-        ?>
+    get_template_part('content-parts/components/info-cover');
+    ?>
     <div class="main_page">
       <div class="page_interna">
         <div class="container full">
           <div class="_2-col">
             <div class="col-1">
-              <div class="tabs_menu notab">
-                <div class="wrapper_collection w-dyn-list">
-                  <div role="list" class="collection_list admin w-dyn-items">
-                    <?php
-                    get_template_part('content-parts/components/sidebar', null, ['sidebar' => $acf_sidebar['sidebar_items']]);
-                  ?>
+              <div style="overflow: auto; max-width: 100%">
+                <div class="tabs_menu notab">
+                  <div class="wrapper_collection w-dyn-list">
+                    <div role="list" class="collection_list admin w-dyn-items">
+                      <?php
+                      get_template_part('content-parts/components/sidebar', null, ['sidebar' => $acf_sidebar['sidebar_items']]);
+                      ?>
+                    </div>
                   </div>
                 </div>
               </div>
               <div class="examen notab">
-                <div class="text_18 bold dark"><?=$acf_examen['title']?></div>
+                <div class="text_18 bold dark"><?= $acf_examen['title'] ?></div>
                 <div class="date_exam notab">
                   <div class="number_date">
-                    <h2 class="_24 carrera"><?=$acf_examen['year']?></h2>
+                    <h2 class="_24 carrera"><?= $acf_examen['year'] ?></h2>
                   </div>
                 </div>
               </div>
@@ -48,8 +50,8 @@ get_header();
                   <div class="content_seccion_tab">
                     <div class="info_section nopadding">
                       <?php
-                              the_content();
-                            ?>
+                      the_content();
+                      ?>
                     </div>
                   </div>
 
