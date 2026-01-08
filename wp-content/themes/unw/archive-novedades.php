0373fc83d2e53c74fd36dcdad2bdd425158120b1
Alejandro M. <alejandr.jacobo@gmail.com>
2026-01-08 12:02:25 -0500

    fix: normalize banner news

--- wp-content/themes/unw/archive-novedades.php
+++ wp-content/themes/unw/archive-novedades.php
@@ -4,7 +4,27 @@
 
  <?php get_template_part(GENERAL_CONTENT_PATH, 'navbar'); ?>
  <main>
-   <?php get_template_part(NEWS_CONTENT_PATH, 'news-hero'); ?>
+   <?php
+   $hero_news = get_field('hero-news', 'option');
+
+   if ($hero_news && is_array($hero_news)) {
+     get_template_part(
+       COMMON_CONTENT_PATH,
+       'hero-slide',
+       [
+         'title'       => $hero_news['title'] ?? 'Noticias',
+         'img_desktop' => $hero_news['images']['desktop']['url'] ?? '',
+         'img_mobile'  => $hero_news['images']['mobile']['url'] ?? '',
+         'alt'         => $hero_news['images']['desktop']['alt'] ?? '',
+         'breadcrumbs' => [
+           ['label' => 'Inicio', 'href' => home_url('/')],
+           ['label' => 'Noticias']
+         ],
+         'variant'     => 'primary',
+       ]
+     );
+   }
+   ?>
    <?php get_template_part(NEWS_TABS_PATH, 'tabs'); ?>
  </main>
  <?php get_footer(); ?>
