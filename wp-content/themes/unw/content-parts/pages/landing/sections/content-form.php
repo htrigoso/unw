13dc87f6b779278f5a37b0463eec23620449a222
Alejandro M. <alejandr.jacobo@gmail.com>
2026-01-15 10:29:44 -0500

    feat: added form landing

---
+++ wp-content/themes/unw/content-parts/pages/landing/sections/content-form.php
@@ -0,0 +1,7 @@
+<section class="x-container x-container--pad-213 content-form-section">
+  <?php
+  get_template_part(COMMON_CONTENT_PATH, 'more-info-form', [
+    'shadow_box' => true
+  ]);
+  ?>
+</section>
