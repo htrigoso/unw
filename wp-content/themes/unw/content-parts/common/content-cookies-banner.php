5c8589a331286239a448b896ef0e8211397f5bb8
Alejandro M. <alejandr.jacobo@gmail.com>
2026-02-02 22:41:47 -0500

    feat: added cookies modal

---
+++ wp-content/themes/unw/content-parts/common/content-cookies-banner.php
@@ -0,0 +1,17 @@
+<?php
+$title = $args['title'] ?? '';
+$message = $args['message'] ?? '';
+$btnText = $args['btnText'] ?? 'Got it!';
+?>
+
+<div class="cookies-banner">
+  <div class="x-container x-container--pad-213 cookies-banner__wrapper">
+    <?php if (!empty($title)): ?>
+      <h3 class="cookies-banner__title"><?= esc_html($title) ?></h3>
+    <?php endif; ?>
+    <?php if (!empty($message)): ?>
+      <div class="cookies-banner__message" data-content="paragraph"><?= $message ?></div>
+    <?php endif; ?>
+    <button class="btn btn-primary btn-xs cookies-banner__btn"><?= esc_html($btnText) ?></button>
+  </div>
+</div>
